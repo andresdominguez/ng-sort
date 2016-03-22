b87811bf00ca2b8f1941b6edf5a9c2358da12128
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-03-22 00:20:47 -0600

    remove unused

--- src/com/andresdominguez/ngsort/SortArgsAction.java
+++ src/com/andresdominguez/ngsort/SortArgsAction.java
@@ -19,7 +19,6 @@ import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
