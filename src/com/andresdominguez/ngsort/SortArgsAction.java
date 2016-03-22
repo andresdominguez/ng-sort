5755674902226881dd19771604eb63a526ba68e8
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-03-21 20:00:17 -0600

    so multiple sort

--- src/com/andresdominguez/ngsort/SortArgsAction.java
+++ src/com/andresdominguez/ngsort/SortArgsAction.java
@@ -3,7 +3,6 @@ package com.andresdominguez.ngsort;
 import com.google.common.collect.Lists;
 import com.intellij.lang.javascript.psi.JSParameterList;
 import com.intellij.lang.javascript.psi.JSProperty;
-import com.intellij.lang.javascript.psi.jsdoc.JSDocTag;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -11,14 +10,17 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class SortArgsAction extends AnAction {
 
@@ -30,30 +32,25 @@ public class SortArgsAction extends AnAction {
       return;
     }
 
-    final CommentAndParamList commentAndParamList = findCommentAndParamList(psiFile);
-    if (commentAndParamList == null) {
+    final List<CommentAndParamList> list = findAllCommentsAndParams(psiFile);
+    if (list.size() == 0) {
       return;
     }
 
-    final NgSorter ngSorter = new NgSorter(editor.getDocument(), commentAndParamList);
+    final Document document = editor.getDocument();
 
     CommandProcessor.getInstance().executeCommand(getEventProject(e), new Runnable() {
       @Override
       public void run() {
-        ngSorter.sort();
+        for (CommentAndParamList commentAndParamList : list) {
+          new NgSorter(document, commentAndParamList).sort();
+        }
       }
     }, "ng sort", null);
   }
 
   @Nullable
-  private CommentAndParamList findCommentAndParamList(PsiFile psiFile) {
-    List<Integer> indices = findNgInjectIndices(psiFile);
-    if (indices.size() == 0) {
-      return null;
-    }
-    int index = indices.get(0);
-
-    PsiElement ngInjectElement = psiFile.findElementAt(index);
+  private CommentAndParamList findCommentAndParamList(PsiElement ngInjectElement) {
     PsiComment comment = PsiTreeUtil.getParentOfType(ngInjectElement, PsiComment.class);
     JSProperty jsProperty = PsiTreeUtil.getParentOfType(ngInjectElement, JSProperty.class);
 
@@ -66,9 +63,23 @@ public class SortArgsAction extends AnAction {
     return new CommentAndParamList(parameterList, comment);
   }
 
+  @NotNull
+  private List<CommentAndParamList> findAllCommentsAndParams(PsiFile psiFile) {
+    List<CommentAndParamList> list = Lists.newArrayList();
+    for (Integer index : findNgInjectIndices(psiFile)) {
+      CommentAndParamList commentAndParamList = findCommentAndParamList(psiFile.findElementAt(index));
+      if (commentAndParamList != null) {
+        list.add(commentAndParamList);
+      }
+    }
+
+    Collections.reverse(list);
+    return list;
+  }
+
   List<Integer> findNgInjectIndices(PsiFile psiFile) {
     String text = psiFile.getText();
-    ArrayList<Integer> indices = Lists.newArrayList();
+    List<Integer> indices = Lists.newArrayList();
 
     int index = text.indexOf("@ngInject");
     while (index != -1) {
