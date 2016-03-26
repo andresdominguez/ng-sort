a890de63a83fc68ef318e3b862b417c5781a05c7
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-03-26 13:02:10 -0400

    introduce goog var

--- src/com/andresdominguez/ngsort/GoogVarAction.java
+++ src/com/andresdominguez/ngsort/GoogVarAction.java
@@ -1,11 +1,21 @@
 package com.andresdominguez.ngsort;
 
+import com.google.common.collect.Iterables;
+import com.intellij.lang.javascript.psi.JSExpressionStatement;
+import com.intellij.lang.javascript.psi.JSLiteralExpression;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GoogVarAction extends AnAction {
 
@@ -17,5 +27,33 @@ public class GoogVarAction extends AnAction {
       return;
     }
 
+    PsiElement currentElement = psiFile.findElementAt(editor.getCaretModel().getOffset());
+    if (currentElement == null) {
+      return;
+    }
+
+    JSExpressionStatement exprStmt = PsiTreeUtil.getParentOfType(currentElement, JSExpressionStatement.class);
+    if (exprStmt == null) {
+      return;
+    }
+
+    final Document document = editor.getDocument();
+    final int offset = exprStmt.getTextOffset();
+    final String varName = getVarName(exprStmt);
+
+    CommandProcessor.getInstance().executeCommand(getEventProject(e), new Runnable() {
+      @Override
+      public void run() {
+        document.replaceString(offset, offset, varName);
+      }
+    }, "ng sort", null);
+  }
+
+  private String getVarName(JSExpressionStatement expressionStatement) {
+    ArrayList<JSLiteralExpression> expressions = new ArrayList<>(
+        PsiTreeUtil.findChildrenOfType(expressionStatement, JSLiteralExpression.class));
+    String text = expressions.get(0).getText();
+    String lastToken = Iterables.getLast(Arrays.asList(text.replaceAll("'", "").split("\\.")));
+    return String.format("var %s = ", lastToken);
   }
 }
