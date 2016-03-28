93cf5ec0cfd51ef861d8f550ca36edd79cbfcac7
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-03-27 20:56:52 -0400

    add error, use runCommand

--- src/com/andresdominguez/ngsort/sort/SortParamListAction.java
+++ src/com/andresdominguez/ngsort/sort/SortParamListAction.java
@@ -1,11 +1,12 @@
 package com.andresdominguez.ngsort.sort;
 
+import com.andresdominguez.ngsort.CommandRunner;
+import com.intellij.codeInsight.hint.HintManager;
 import com.intellij.lang.javascript.psi.JSParameterList;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiElement;
@@ -25,15 +26,16 @@ public class SortParamListAction extends AnAction {
     PsiElement element = psiFile.findElementAt(editor.getCaretModel().getOffset());
     final JSParameterList parameterList = PsiTreeUtil.getParentOfType(element, JSParameterList.class);
     if (parameterList == null) {
+      HintManager.getInstance().showErrorHint(editor, "Can't find parameter list");
       return;
     }
 
     final Document document = editor.getDocument();
-    CommandProcessor.getInstance().executeCommand(getEventProject(e), new Runnable() {
+    CommandRunner.runCommand(getEventProject(e), new Runnable() {
       @Override
       public void run() {
         NgSorter.sortFunctionArgs(parameterList, document);
       }
-    }, "ng sort", null);
+    });
   }
 }
